5f95f44c749359ed7d03717011e09335a68f9909
João Cruz <132522413+Pitucooh@users.noreply.github.com>
2023-11-16 11:47:10 -0300

    sd

--- backend.js
+++ backend.js
@@ -10,17 +10,6 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-
-let filmes = [
-    {
-    titulo: "Forrest Gump - O Contador de Histórias",
-    sinopse: "Quarenta anos da história dos Estados Unidos, vistos pelos olhos de Forrest Gump (Tom Hanks),um rapaz com QI abaixo da média e boas intenções."
-    },
-    {
-    titulo: "Um Sonho de Liberdade",
-    sinopse: "Em 1946, Andy Dufresne (Tim Robbins), um jovem e bem sucedido banqueiro, tem a sua vida radicalmente modificada ao ser condenado por um crime que nunca cometeu, o homicídio de sua esposa e do amante dela"
-    }
-]
 async function conectarMongoDB(){
     await mongoose.connect(`mongodb+srv://pituco:mznxbcvg2@pitucooh.8ecyqf5.mongodb.net/?retryWrites=true&w=majority`)
 }
